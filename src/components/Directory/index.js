31a27eded36af8959fd779a7bd75676d8d302e7a
CPKVG <51846930+CPKVG@users.noreply.github.com>
2021-06-08 06:34:35 +1200

    init upload

---
+++ src/components/Directory/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './styles.scss';
+
+const Directory = props => {
+  return (
+    <div className="directory">
+        <p>HOMEPAGE</p>
+    </div>
+  );
+};
+
+export default Directory;
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = props => {
+    return (
+      <header className="footer">
+        <div className="wrap">
+            chat app 
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header;
